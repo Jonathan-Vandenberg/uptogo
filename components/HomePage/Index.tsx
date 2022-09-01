b0c468c7ac7ebe2f75d2baddc79e1cc108c32a6b
jon-vdb <jonvdberg8@gmail.com>
2022-09-01 19:58:28 +0700

    ral first commit

--- components/HomePage/Index.tsx
+++ components/HomePage/Index.tsx
@@ -1,9 +1,12 @@
 import HeroSlides from "../../components/UI/HeroSlides";
 import Stationary from "../UI/Stationary";
+import Image from "next/image";
+import landingImage from "../../public/landingImage1.png";
 
 export default function App() {
   return (
     <>
+      {/* <Image src={landingImage} width={375} height={812} alt="landing Image" /> */}
       <Stationary />
       <HeroSlides />
     </>
