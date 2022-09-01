6b5dbae06991380c13f459097a0406e954568ac5
jon-vdb <jonvdberg8@gmail.com>
2022-09-01 20:08:28 +0700

    landing image

--- components/HomePage/Index.tsx
+++ components/HomePage/Index.tsx
@@ -1,13 +1,19 @@
 import HeroSlides from "../../components/UI/HeroSlides";
 import Stationary from "../UI/Stationary";
 import Image from "next/image";
-import landingImage from "../../public/landingImage1.png";
+import landingImage2 from "../../public/landingImage2.png";
 
 export default function App() {
   return (
     <>
       {/* <Image src={landingImage} width={375} height={812} alt="landing Image" /> */}
-      <Stationary />
+      <Image
+        src={landingImage2}
+        alt="lading image"
+        width={390}
+        height={844}
+        layout="responsive"
+      />
       <HeroSlides />
     </>
   );
--- components/UI/Stationary.tsx
+++ components/UI/Stationary.tsx
@@ -2,7 +2,7 @@ import Spline from "@splinetool/react-spline";
 
 export default function App() {
   return (
-    <div className="md:hidden">
+    <div className="md:hidden w-full object-fit">
       <Spline scene="https://prod.spline.design/PQGMYgBC3borPOcN/scene.splinecode" />
     </div>
   );
