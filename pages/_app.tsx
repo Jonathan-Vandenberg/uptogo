b77ff330211f293010e4e6f14d470ad3aa5ce498
jon-vdb <jonvdberg8@gmail.com>
2022-09-02 11:27:30 +0700

    splash animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import SideNavModal from "../components/Global/Navbars/SideNavModal";
 import Footer from "../components/Global/Navbars/Footer/Footer";
 import Loading3D from "../components/UI/Loading3D";
 import { motion, useAnimationControls } from "framer-motion";
+import Spline from "@splinetool/react-spline";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const controls = useAnimationControls();
@@ -17,17 +18,24 @@ function MyApp({ Component, pageProps }: AppProps) {
     },
   });
 
-  controlsBody.start({
-    opacity: 1,
-    transition: {
-      delay: 4.1,
-    },
-  });
+  const handleLoad = () => {
+    controlsBody.start({
+      opacity: 1,
+      transition: {
+        delay: 2,
+      },
+    });
+  };
 
   return (
     <div className="realtive">
       <motion.div initial={{ opacity: 1 }} animate={controls}>
-        <Loading3D />
+        <div className="z-50 absolute top-0 left-0">
+          <Spline
+            onLoad={handleLoad}
+            scene="https://prod.spline.design/gg0modFcZyGWPqfY/scene.splinecode"
+          />
+        </div>
       </motion.div>
       <motion.div initial={{ opacity: 0 }} animate={controlsBody}>
         <Navbar />
