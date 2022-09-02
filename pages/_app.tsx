da0ac85ea09f4ab19f511931f662ca3d70c7b7bf
jon-vdb <jonvdberg8@gmail.com>
2022-09-02 11:45:35 +0700

    splash animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,37 +6,45 @@ import Footer from "../components/Global/Navbars/Footer/Footer";
 import Loading3D from "../components/UI/Loading3D";
 import { motion, useAnimationControls } from "framer-motion";
 import Spline from "@splinetool/react-spline";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [splashComplete, setSplashComplete] = useState(false);
   const controls = useAnimationControls();
   const controlsBody = useAnimationControls();
 
-  controls.start({
-    opacity: 0,
-    transition: {
-      delay: 4,
-    },
-  });
-
   const handleLoad = () => {
+    controls.start({
+      opacity: 0,
+      transition: {
+        duration: 0.5,
+        delay: 4,
+      },
+    });
     controlsBody.start({
       opacity: 1,
       transition: {
-        delay: 2,
+        duration: 0.5,
+        delay: 4,
       },
     });
+    setTimeout(() => {
+      setSplashComplete(true);
+    }, 4200);
   };
 
   return (
     <div className="realtive">
-      <motion.div initial={{ opacity: 1 }} animate={controls}>
-        <div className="z-50 absolute top-0 left-0">
-          <Spline
-            onLoad={handleLoad}
-            scene="https://prod.spline.design/gg0modFcZyGWPqfY/scene.splinecode"
-          />
-        </div>
-      </motion.div>
+      {!splashComplete && (
+        <motion.div initial={{ opacity: 1 }} animate={controls}>
+          <div className="z-50 absolute top-0 left-0">
+            <Spline
+              onLoad={handleLoad}
+              scene="https://prod.spline.design/gg0modFcZyGWPqfY/scene.splinecode"
+            />
+          </div>
+        </motion.div>
+      )}
       <motion.div initial={{ opacity: 0 }} animate={controlsBody}>
         <Navbar />
         <SideNavModal />
