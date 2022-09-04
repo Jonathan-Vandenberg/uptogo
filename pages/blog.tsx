8ffa58a3cd21e5ad7d4d66846222b2b8562ebdfe
jon-vdb <jonvdberg8@gmail.com>
2022-09-04 22:17:04 +0700

    vercel

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function Blog() {
   const { data, loading, error } = useBlogPostQuery({
     variables: {
-      id: "6314ab424504dd6cae765ffa",
+      id: "6314be035b996f218b45c1c6",
     },
   });
   return <BlogPostMarkup data={data} />;
