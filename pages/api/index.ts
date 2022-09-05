2f366ec54f7b1b620f6c1834f4ec8d506d005699
jon-vdb <jonvdberg8@gmail.com>
2022-09-05 17:04:16 +0700

    courses

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -5,7 +5,6 @@ import prisma from '../../lib/prisma'
 import type { PrismaClient } from '@prisma/client'
 import { Resolvers } from '../../types'
 import { NextApiRequest, NextApiResponse } from "next"
-import { MdInfoOutline } from "react-icons/md"
 
 export async function createContext(): Promise<GraphQLContext>{
   return { prisma }
@@ -43,41 +42,31 @@ const resolvers: Resolvers = {
         tableContents3: input?.tableContents3,
         tableContents4: input?.tableContents4,
         tableContents5: input?.tableContents5,
-
         p1: input?.p1,
         p2: input?.p2,
         p3: input?.p3,
-
         subtitle2: input?.subtitle2,
-
         p4: input?.p4,
         p5: input?.p5,
-
         l1: input?.l1,
         l2: input?.l2,
         l3: input?.l3,
         l4: input?.l4,
         l5: input?.l5,
-
         subtitle3: input?.subtitle3,
-
         p6: input?.p6,
         p7: input?.p7,
-
         conclusion1: input?.conclusion1,
         conclusion2: input?.conclusion2,
         conclusion3: input?.conclusion3,
-
         reference1: input?.reference1,
         reference2: input?.reference2,
         reference3: input?.reference3,
         reference4: input?.reference4,
         reference5: input?.reference5,
-
         authorName: input?.authorName,
         authorAbout: input?.authorAbout,
         authorLink: input?.authorLink,
-
         photoCredit: input?.photoCredit,
         editedBy: input?.editedBy,
         mainImage: input?.mainImage,
