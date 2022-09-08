3772ec086deeb0a97f19b484900cab2977c3430a
jon-vdb <jonvdberg8@gmail.com>
2022-09-08 21:33:16 +0700

    dynamic routing

--- components/UI/Popup.tsx
+++ components/UI/Popup.tsx
@@ -40,7 +40,7 @@ export default function PopupCard() {
             </li>
           </ul>
           <button className="px-4 py-2 rounded-full text-white bg-orange">
-            <div className="items-center justify-center flex space-x-2">
+            <div className="items-center justify-center flex space-x-">
               <p>Start Now</p>
               <BsArrowRight />
             </div>
