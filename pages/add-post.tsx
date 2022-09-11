7a787c06294639e5c7b5f54c749e7a365ca23543
jon-vdb <jonvdberg8@gmail.com>
2022-09-11 12:54:39 +0700

    added software development main story and articles routing

--- pages/add-post.tsx
+++ pages/add-post.tsx
@@ -1,7 +1,37 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useState } from "react";
 import AddPostForm from "../components/MainForm";
+import { useSoftwareDevelopmentQuery } from "../types";
 
 const NewSchool = () => {
+  const [showForm, setShowForm] = useState(false);
+  const [edit, setEdit] = useState(false);
+  const [add, setAdd] = useState(false);
+
+  const { asPath } = useRouter();
+
+  function Crubs() {
+    return <p>home{asPath}</p>;
+  }
+
+  const handleAdd = () => {
+    setShowForm(!showForm);
+    setAdd(true);
+    setEdit(false);
+  };
+
+  const handleEdit = () => {
+    setShowForm(!showForm);
+    setAdd(false);
+    setEdit(true);
+  };
+
+  const { data } = useSoftwareDevelopmentQuery({
+    variables: {
+      id: "631d68109f9c5ab336b3d04e",
+    },
+  });
   return (
     <>
       <Head>
@@ -11,12 +41,10 @@ const NewSchool = () => {
       </Head>
       <div className="mt-4 md:mt-20 bg-red-100 w-full md:w-2/3 mx-auto">
         <AddPostForm
-          details={undefined}
-          add={false}
-          edit={false}
-          handleClose={function (): void {
-            throw new Error("Function not implemented.");
-          }}
+          details={data?.softwareDevelopment}
+          add={add}
+          edit={edit}
+          handleClose={() => setShowForm(false)}
         />
       </div>
     </>
