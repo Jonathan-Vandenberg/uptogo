ec51d8b46d2ce68a85700e9fc47f6199d9881172
jon-vdb <jonvdberg8@gmail.com>
2022-09-15 22:41:06 +0700

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {
       colors: {
         navAdvertise: "rgb(235,250,239)",
