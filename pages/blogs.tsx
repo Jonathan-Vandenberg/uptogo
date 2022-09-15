f718c7c8adf439fb29d7bd2524adddfef4287294
jon-vdb <jonvdberg8@gmail.com>
2022-09-15 18:41:30 +0700

    Blog Params

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,15 +1,30 @@
+import { PrismaClient } from "@prisma/client";
 import BlogPageHero from "../components/Blog/BlogPageHero";
-import { useBlogCardQuery } from "../types";
+import { BlogPost } from "../types";
 
-export default function App() {
-  const { data, loading, error } = useBlogCardQuery();
+interface IProps {
+  data: BlogPost[];
+}
+
+export default function App({ data }: IProps) {
   return (
     <div className="flex flex-col items-center justify-center space-y-12">
-      {data?.blogCard?.map((post) => (
+      {data?.map((post) => (
         <div key={post?.id} className="w-full">
           <BlogPageHero data={post} />
         </div>
       ))}
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const prisma = new PrismaClient();
+  const data = await prisma.blogPost.findMany();
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
