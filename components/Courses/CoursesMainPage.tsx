df93650af77a4e2eea4eb1363dc9605e63624c2d
jon-vdb <jonvdberg8@gmail.com>
2022-09-15 11:58:19 +0700

    styling the courses cards

--- components/Courses/CoursesMainPage.tsx
+++ components/Courses/CoursesMainPage.tsx
@@ -43,13 +43,8 @@ export default function BlogMain({ data, handleEdit, handleAdd }: IProps) {
   return (
     <div className="container max-w-[678px] md:max-w-[900px] mx-auto md:pt-12 pb-8">
       <div className="pb-6 px-6 md:px-0 md:pb-10">
-        <div>
-          <div className="logoFont text-darkBlue text-2xl md:text-3xl lg:text-4xl pb-6">
-            {data?.title}
-          </div>
-          <div className="text-gray-700 text-xl pb-3 md:text-xl lg:text-2xl">
-            {data?.subtitle1}
-          </div>
+        <div className="logoFont text-darkBlue text-2xl md:text-3xl lg:text-4xl pb-6">
+          {data?.title}
         </div>
       </div>
       <div className="pb-8">
@@ -60,6 +55,11 @@ export default function BlogMain({ data, handleEdit, handleAdd }: IProps) {
           alt="image"
           layout="responsive"
         />
+      </div>
+      <div className="pb-6 px-6 md:px-0 md:pb-10">
+        <div className="text-gray-700 text-xl pb-3 md:text-xl lg:text-2xl">
+          {data?.subtitle1}
+        </div>
       </div>
       <div className="px-5 md:px-0">
         <BlogPostMarkup
