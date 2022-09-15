0cec3e483e4a73c91cef0e957fefcd6b945229e2
jon-vdb <jonvdberg8@gmail.com>
2022-09-15 22:48:46 +0700

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,9 @@ module.exports = {
   theme: {
     container: {
       padding: {
-        DEFAULT: "1rem",
-        sm: "2rem",
         lg: "4rem",
-        xl: "5rem",
-        "2xl": "6rem",
+        xl: "8rem",
+        "2xl": "10rem",
       },
     },
     extend: {
