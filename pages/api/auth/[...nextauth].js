aecf16c9e777de9eee8857489c4d51ee0e1592ca
jon-vdb <jonvdberg8@gmail.com>
2022-09-16 09:42:20 +0700

    email login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
+import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
@@ -16,6 +17,11 @@ export default NextAuth({
     FacebookProvider({
       clientId: String(process.env.FACEBOOK_CLIENT_ID),
       clientSecret: String(process.env.FACEBOOK_CLIENT_SECRET),
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+      // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
     }),
   ],
 });
