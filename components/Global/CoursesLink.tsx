8a09a6a08cb86aed3acc87ae6a5d17805643f031
jon-vdb <jonvdberg8@gmail.com>
2022-09-18 20:29:18 +0700

    mobile nav

--- components/Global/CoursesLink.tsx
+++ components/Global/CoursesLink.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsNewspaper } from "react-icons/bs";
 import {
@@ -18,7 +19,7 @@ import {
 
 export default function App() {
   return (
-    <div className="p-2 fixed bg-darkBlue rounded-full bottom-7 z-50 right-7 md:hidden">
+    <div className="p-2 fixed bg-darkBlue shadow-lg rounded-full bottom-5 z-50 right-5 md:hidden">
       <div className="flex items-center justify-center space-x-8 px-4">
         <Link href={"/courses"}>
           <FaGraduationCap color="white" size={35} />
@@ -26,9 +27,18 @@ export default function App() {
         <a href="mailto: info@uptogo.org">
           <MdEmail color="white" size={35} />
         </a>
-        <a href="tel:+84926561080">
-          <MdCall color="white" size={35} />
-        </a>
+        <motion.div
+          initial={{ rotateX: 0, rotateY: 0 }}
+          animate={{
+            rotateX: 360,
+            rotateY: 360,
+            transition: { duration: 2, repeat: 6, repeatDelay: 20 },
+          }}
+        >
+          <a href="tel:+84926561080">
+            <MdCall color="white" size={35} />
+          </a>
+        </motion.div>
       </div>
     </div>
   );
