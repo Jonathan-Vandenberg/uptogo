086608f2b4ad383275a5e6b3fe091a4b0a0ca71f
jon-vdb <jonvdberg8@gmail.com>
2022-09-18 08:30:51 +0700

    blog card spacing

---
+++ lib/links.ts
@@ -0,0 +1,38 @@
+export const designLinks = [
+  {
+    course: "Acting",
+    link: "/courses/design/acting",
+  },
+  {
+    course: "3d Animation",
+    link: "/courses/design/animation3d",
+  },
+  {
+    course: "Digital Media",
+    link: "/courses/design/digital-media",
+  },
+  {
+    course: "Fashion Design",
+    link: "/courses/design/fashion-design",
+  },
+  {
+    course: "Film",
+    link: "/courses/design/film",
+  },
+  {
+    course: "Graphic Design",
+    link: "/courses/design/graphic-design",
+  },
+  {
+    course: "Interior Design",
+    link: "/courses/design/interior-design",
+  },
+  {
+    course: "Landscaping",
+    link: "/courses/design/landscape",
+  },
+  {
+    course: "Music & Audio",
+    link: "/courses/design/music",
+  },
+];
--- pages/courses/design/index.tsx
+++ pages/courses/design/index.tsx
@@ -7,45 +7,7 @@ import CoursesNarrowCards from "../../../components/Courses/CoursesNarrowCards";
 import MainForm from "../../../components/MainForm";
 import prisma from "../../../lib/prisma";
 import { Design } from "../../../types";
-
-const cardData = [
-  {
-    course: "Acting",
-    link: "/courses/design/acting",
-  },
-  {
-    course: "3d Animation",
-    link: "/courses/design/animation3d",
-  },
-  {
-    course: "Digital Media",
-    link: "/courses/design/digital-media",
-  },
-  {
-    course: "Fashion Design",
-    link: "/courses/design/fashion-design",
-  },
-  {
-    course: "Film",
-    link: "/courses/design/film",
-  },
-  {
-    course: "Graphic Design",
-    link: "/courses/design/graphic-design",
-  },
-  {
-    course: "Interior Design",
-    link: "/courses/design/interior-design",
-  },
-  {
-    course: "Landscaping",
-    link: "/courses/design/landscape",
-  },
-  {
-    course: "Music & Audio",
-    link: "/courses/design/music",
-  },
-];
+import { designLinks } from "../../../lib/links";
 
 interface IProps {
   data: Design;
@@ -122,7 +84,7 @@ export default function App({ data }: IProps) {
         </div>
       )}
       {data ? (
-        <CoursesNarrowCards cardData={cardData} />
+        <CoursesNarrowCards cardData={designLinks} />
       ) : (
         <div>
           <p>Loading...</p>
