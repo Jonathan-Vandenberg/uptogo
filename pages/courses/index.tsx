f2a23697c472b3f9ba247c1a8aa71442a4b8f93d
jon-vdb <jonvdberg8@gmail.com>
2022-09-18 12:56:09 +0700

    icons

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -2,6 +2,12 @@ import Link from "next/link";
 import Image from "next/image";
 import it from "../../public/courseMain/it/Data_Management.jpeg";
 import { useRouter } from "next/router";
+import healthIcon from "../../public/courseIcons/healthcare.png";
+import managementIcon from "../../public/courseIcons/management.png";
+import designIcon from "../../public/courseIcons/web-design.png";
+import hospitalityIcon from "../../public/courseIcons/hospitality.png";
+import tradeIcon from "../../public/courseIcons/competence.png";
+import itIcon from "../../public/courseIcons/computer.png";
 
 export default function App() {
   const router = useRouter();
@@ -11,16 +17,10 @@ export default function App() {
         className="flex items-center justify-center flex-col cursor-pointer"
         onClick={() => router.push("/courses/information-technology")}
       >
-        <div className="w-full h-full relative">
-          <Image
-            src={it}
-            alt="IT image"
-            layout="fill" // required
-            objectFit="cover" // change to suit your needs
-            priority
-          />
+        <div className="w-60 h-full items-center flex justify-center">
+          <Image src={itIcon} alt="IT image" width={60} height={60} priority />
         </div>
-        <div className="text-lg text-gray-700 bg-sky-100 w-full text-center py-2">
+        <div className="text-lg text-gray-700 w-full text-center py-2">
           Công nghệ thông tin
         </div>
       </div>
@@ -28,16 +28,10 @@ export default function App() {
         className="flex items-center justify-center flex-col cursor-pointer"
         onClick={() => router.push("/courses/management")}
       >
-        <div className="w-full h-full relative">
-          <Image
-            src={it}
-            alt="management image"
-            layout="fill" // required
-            objectFit="cover" // change to suit your needs
-            priority
-          />
+        <div className="w-60 h-full items-center flex justify-center">
+          <Image src={itIcon} alt="IT image" width={60} height={60} priority />
         </div>
-        <div className="text-lg text-gray-700 bg-sky-100 w-full text-center py-2">
+        <div className="text-lg text-gray-700 w-full text-center py-2">
           Quản lý
         </div>
       </div>
@@ -45,16 +39,16 @@ export default function App() {
         className="flex items-center justify-center flex-col cursor-pointer"
         onClick={() => router.push("/courses/design")}
       >
-        <div className="w-full h-full relative">
+        <div className="w-60 h-full items-center flex justify-center">
           <Image
-            src={it}
-            alt="design image"
-            layout="fill" // required
-            objectFit="cover" // change to suit your needs
+            src={designIcon}
+            alt="IT image"
+            width={60}
+            height={60}
             priority
           />
         </div>
-        <div className="text-lg text-gray-700 bg-sky-100 w-full text-center py-2">
+        <div className="text-lg text-gray-700 w-full text-center py-2">
           Thiết kế
         </div>
       </div>
@@ -62,16 +56,16 @@ export default function App() {
         className="flex items-center justify-center flex-col cursor-pointer"
         onClick={() => router.push("/courses/health")}
       >
-        <div className="w-full h-full relative">
+        <div className="w-60 h-full items-center flex justify-center">
           <Image
-            src={it}
-            alt="health image"
-            layout="fill" // required
-            objectFit="cover" // change to suit your needs
+            src={healthIcon}
+            alt="IT image"
+            width={60}
+            height={60}
             priority
           />
         </div>
-        <div className="text-lg text-gray-700 bg-sky-100 w-full text-center py-2">
+        <div className="text-lg text-gray-700 w-full text-center py-2">
           Sức khỏe
         </div>
       </div>
@@ -79,16 +73,16 @@ export default function App() {
         className="flex items-center justify-center flex-col cursor-pointer"
         onClick={() => router.push("/courses/hospitality")}
       >
-        <div className="w-full h-full relative">
+        <div className="w-60 h-full items-center flex justify-center">
           <Image
-            src={it}
-            alt="hospitality image"
-            layout="fill" // required
-            objectFit="cover" // change to suit your needs
+            src={hospitalityIcon}
+            alt="IT image"
+            width={60}
+            height={60}
             priority
           />
         </div>
-        <div className="text-lg text-gray-700 bg-sky-100 w-full text-center py-2">
+        <div className="text-lg text-gray-700 w-full text-center py-2">
           Nhà hàng khách sạn
         </div>
       </div>
@@ -96,16 +90,16 @@ export default function App() {
         className="flex items-center justify-center flex-col cursor-pointer"
         onClick={() => router.push("/courses/trades")}
       >
-        <div className="w-full h-full relative">
+        <div className="w-60 h-full items-center flex justify-center">
           <Image
-            src={it}
-            alt="trades image"
-            layout="fill" // required
-            objectFit="cover" // change to suit your needs
+            src={tradeIcon}
+            alt="IT image"
+            width={60}
+            height={60}
             priority
           />
         </div>
-        <div className="text-lg text-gray-700 bg-sky-100 w-full text-center py-2">
+        <div className="text-lg text-gray-700 w-full text-center py-2">
           Thương mại & Thể thao
         </div>
       </div>
