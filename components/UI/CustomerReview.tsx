4960137f716191006d84b7bef3c124e58f217443
jon-vdb <jonvdberg8@gmail.com>
2022-09-19 20:18:42 +0700

    dynamic contact button

--- components/UI/CustomerReview.tsx
+++ components/UI/CustomerReview.tsx
@@ -1,7 +1,5 @@
 import { Avatar, Rating } from "@mui/material";
-import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 
 type Review = {
@@ -25,18 +23,8 @@ export default function MediaCard({ customer }: { customer: Review[] }) {
               <Rating name="read-only" value={5} readOnly />
             </div>
             <p className="py-3 text-2xl font-semibold">{item.name}</p>
-            <p className="line-clamp-3">{item.review}</p>
+            <p className="line-clamp-5">{item.review}</p>
           </CardContent>
-          <CardActions>
-            <div className="flex w-full items-center justify-between p-2">
-              <Button size="small" className="text-iwanttoColor">
-                Share
-              </Button>
-              <Button size="small" className="text-iwanttoColor">
-                More Reviews
-              </Button>
-            </div>
-          </CardActions>
         </Card>
       ))}
     </div>
