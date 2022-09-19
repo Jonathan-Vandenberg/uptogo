8a6ae66a6ece91b3c67823e70fd896c0d8af461b
jon-vdb <jonvdberg8@gmail.com>
2022-09-19 20:52:42 +0700

    auth loop

--- pages/login.tsx
+++ pages/login.tsx
@@ -15,12 +15,12 @@ export default function Login() {
       router.push("/");
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [session?.user]);
 
   return (
     <div className="relative">
       <div className="absolute top-0 left-0 w-screen h-screen bg-white z-40 ">
-        <div className="flex flex-col items-center justify-center space-y-2 mt-20">
+        <div className="flex flex-col items-center justify-center space-y-2 mt-12">
           <div className="pb-8">
             <Image src={logo} width={240} height={240} alt="signin logo" />
           </div>
