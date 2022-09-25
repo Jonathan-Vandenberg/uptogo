b7f8a384fb83ec0233c76d79ac6669a5e3c54024
jon-vdb <jonvdberg8@gmail.com>
2022-09-25 22:14:05 +0700

    course pages

--- components/Courses/CoursesDesktopPage.tsx
+++ components/Courses/CoursesDesktopPage.tsx
@@ -1,9 +1,13 @@
 import { AnimatePresence, motion } from "framer-motion";
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
-import { BsArrowDownRightSquare, BsCircleFill } from "react-icons/bs";
-import heroImage from "../../public/courseMain/CouresesHomeImage.jpg";
-import ITDesktop from "../../public/courseMain/ITDesktop.jpg";
+import { BsCircleFill } from "react-icons/bs";
+import ITDesktop from "../../public/courseMain/it.jpg";
+import hospitalityDesktop from "../../public/courseMain/hospitality.jpg";
+import tradesDesktop from "../../public/courseMain/trades.jpg";
+import healthDesktop from "../../public/courseMain/health.jpg";
+import managementDesktop from "../../public/courseMain/management.jpg";
+import designDesktop from "../../public/courseMain/design.jpg";
 import landmarks from "../../public/heroImages/heroTravel.jpg";
 import { useAppSelector } from "../../redux-hooks/hooks";
 import hero from "../../public/courseMain/couresesDesktopMain.jpg";
@@ -37,7 +41,6 @@ export default function App({
 }: IProps) {
   const translate = useAppSelector((state) => state.translationState.translate);
   const color = useAppSelector((state) => state.iconState.color);
-
   return (
     <>
       <div className="snap-y h-screen overflow-y-scroll snap-mandatory">
@@ -58,7 +61,7 @@ export default function App({
           title={"Công nghệ thông tin"}
           Etitle={"Information Technology"}
           translate={translate}
-          bgColor="purple-200"
+          bgColor="purple1"
           paragraph1={it?.p1}
           list1={it?.l1}
           list2={it?.l2}
@@ -70,61 +73,61 @@ export default function App({
           title={"Quản lý"}
           Etitle={"Management"}
           translate={translate}
-          bgColor="red-200"
+          bgColor="blue1"
           paragraph1={management?.p1}
           list1={management?.l1}
           list2={management?.l2}
           list3={management?.l3}
           link={"/courses/management"}
-          image={ITDesktop}
+          image={managementDesktop}
         />
         <Category
           title={"Thiết kế"}
           Etitle={"Design"}
           translate={translate}
-          bgColor="yellow-200"
+          bgColor="yellow"
           paragraph1={design?.p1}
           list1={design?.l1}
           list2={design?.l2}
           list3={design?.l3}
           link={"/courses/design"}
-          image={ITDesktop}
+          image={designDesktop}
         />
         <Category
           title={"Sức khỏe"}
           Etitle={"Health"}
           translate={translate}
-          bgColor="green-200"
+          bgColor="red"
           paragraph1={health?.p1}
           list1={health?.l1}
           list2={health?.l2}
           list3={health?.l3}
           link={"/courses/health"}
-          image={ITDesktop}
+          image={healthDesktop}
         />
         <Category
           title={"Nhà hàng khách sạn"}
           Etitle={"Hospitality"}
           translate={translate}
-          bgColor="orange-200"
+          bgColor="purple2"
           paragraph1={hospitality?.p1}
           list1={hospitality?.l1}
           list2={hospitality?.l2}
           list3={hospitality?.l3}
           link={"/courses/hospitality"}
-          image={ITDesktop}
+          image={hospitalityDesktop}
         />
         <Category
           title={"Thương mại & Thể thao"}
           Etitle={"Trades & Sports"}
           translate={translate}
-          bgColor="blue-200"
+          bgColor="blue2"
           paragraph1={trades?.p1}
           list1={trades?.l1}
           list2={trades?.l1}
           list3={trades?.l1}
           link={"/courses/trades"}
-          image={ITDesktop}
+          image={tradesDesktop}
         />
         {color && (
           <div className="fixed bottom-12 left-0 w-screen">
@@ -167,9 +170,49 @@ const Category = ({
   link,
   image,
 }: ICategory) => {
+  const backgroundClasses = {
+    purple1:
+      "snap-start h-screen flex items-center justify-center flex-col bg-gradient-to-r from-purple-200 via-white to-white lg:flex-row w-full",
+    blue1:
+      "snap-start h-screen flex items-center justify-center flex-col bg-gradient-to-r from-sky-200 via-white to-white lg:flex-row w-full",
+    yellow:
+      "snap-start h-screen flex items-center justify-center flex-col bg-gradient-to-r from-yellow-300 via-white to-white lg:flex-row w-full",
+    red: "snap-start h-screen flex items-center justify-center flex-col bg-gradient-to-r from-red-300 via-white to-white lg:flex-row w-full",
+    purple2:
+      "snap-start h-screen flex items-center justify-center flex-col bg-gradient-to-r from-purple-300 via-white to-white lg:flex-row w-full",
+    blue2:
+      "snap-start h-screen flex items-center justify-center flex-col bg-gradient-to-r from-sky-300 via-white to-white lg:flex-row w-full",
+  };
+
+  let style;
+
+  if (bgColor === "purple1") {
+    style = backgroundClasses.purple1;
+  }
+
+  if (bgColor === "blue1") {
+    style = backgroundClasses.blue1;
+  }
+
+  if (bgColor === "yellow") {
+    style = backgroundClasses.yellow;
+  }
+
+  if (bgColor === "red") {
+    style = backgroundClasses.red;
+  }
+
+  if (bgColor === "purple2") {
+    style = backgroundClasses.purple2;
+  }
+
+  if (bgColor === "blue2") {
+    style = backgroundClasses.blue2;
+  }
+
   return (
     <AnimatePresence>
-      <div className="snap-start h-screen flex items-center justify-center flex-col lg:flex-row w-full">
+      <div className={style}>
         <motion.div
           key="Content"
           initial={{ x: -180 }}
@@ -184,7 +227,7 @@ const Category = ({
             </Link>
           </div>
           <div className=" flex flex-col items-center justify-center space-y-6">
-            <p className="text-lg line-clamp-6">{paragraph1}</p>
+            <p className="text-xl line-clamp-6">{paragraph1}</p>
             <ul className="flex items-start justify-center flex-col space-y-3">
               <div className="flex items-center justify-center space-x-3">
                 <div>
@@ -222,7 +265,7 @@ const Category = ({
           }}
           className="h-full max-w-[600px] lg:w-1/2"
         >
-          <div className="lg:mt-40 block">
+          <div className="block">
             <Image src={image as string} alt="IT image" layout="responsive" />
           </div>
         </motion.div>
