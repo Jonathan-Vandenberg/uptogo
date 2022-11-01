b49d3d5022e23606f55da0f0561b4cc19f10e081
jon-vdb <jonvdberg8@gmail.com>
2022-11-01 18:08:37 +0700

    Admin and SEO info in form 2

--- components/MainForm.tsx
+++ components/MainForm.tsx
@@ -625,11 +625,11 @@ const AddClientForm = ({ details, add, edit, handleClose }: IProps) => {
   return (
     <div className="px-5 max-w-[678px] md:max-w-[900px] mx-auto">
       <div className="flex items-center justify-center">
-        <p className="text-2xl text-gray-700 font-bold text-center p-3">
+        <p className="text-3xl text-gray-700 font-bold text-center underline underline-offset-2">
           ADMIN
         </p>
       </div>
-      <form onSubmit={onFinish} className="space-y-3 py-12">
+      <form onSubmit={onFinish} className="space-y-3 py-6">
         {/* {category === "ADVICE" ||
           ("GUIDANCE" && (
             <div className="px-8 ">
